6b97ed5470a47ff4e1f623517e80cc0baa9de21d
Jinzhou Zhang <lotabout@gmail.com>
2020-02-23 15:38:54 +0800

    Feature: ensure Send + Sync for matcher

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,7 @@ pub mod clangd;
 pub mod skim;
 mod util;
 
-pub trait FuzzyMatcher {
+pub trait FuzzyMatcher: Send + Sync {
     /// fuzzy match choice with pattern, and return the score & matched indices of characters
     fn fuzzy_indices(&self, choice: &str, pattern: &str) -> Option<(i64, Vec<usize>)>;
 
